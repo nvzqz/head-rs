b5834ee5ec8aafc807b6f7bae16444263989a65d
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-11-18 21:04:49 -0500

    Create initial `head` crate

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Common types with inline headers, brought to you by
+//! [@NikolaiVazquez](https://twitter.com/NikolaiVazquez).
+//!
+//! ## License
+//!
+//! This project is released under either the
+//! [MIT License](https://github.com/nvzqz/head-rs/blob/main/LICENSE-MIT) or
+//! [Apache License (Version 2.0)](https://github.com/nvzqz/head-rs/blob/main/LICENSE-APACHE),
+//! at your choosing.
+
+#![cfg_attr(not(feature = "std"), no_std)]
